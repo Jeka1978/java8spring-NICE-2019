421be00222c584a7db5535d346f28feb843156c5
jeka1978 <kit2009>
2019-12-30 09:52:35 +0200

    Initial commit

---
+++ src/main/java/java8_labs/ListUtils.java
@@ -0,0 +1,18 @@
+package java8_labs;
+
+import java.util.List;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class ListUtils {
+    public static <T> int countDuplicates(T t, List<T> list) {
+        int counter=0;
+        for (T t1 : list) {
+            if (t1.equals(t)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/java8_labs/ListUtilsTest.java
@@ -0,0 +1,22 @@
+package java8_labs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class ListUtilsTest {
+
+    @Test
+    public void countDuplicates() {
+        List<String> list = asList("java", "Java", "JAVA", "scala");
+        int duplicates = ListUtils.countDuplicates("java", list);
+        Assert.assertEquals(3,duplicates);
+    }
+}
